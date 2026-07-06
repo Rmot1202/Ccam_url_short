cd27d15a9cdf9d9f7037c00ca482994f3019b209
Raven Mott <Raven.Mott@CCAM-VA.COM>
2026-07-06 16:42:45 -0400

    install

--- API/app/celery_app.py
+++ API/app/celery_app.py
@@ -3,6 +3,6 @@ from celery import Celery
 
 celery_app = Celery(
     "url_shortener",
-    broker=os.getenv("REDIS_URL", "redis://localhost:6379/0"),
-    backend=os.getenv("REDIS_URL", "redis://localhost:6379/1"),
+    broker=os.getenv("REDIS_URL"),
+    backend=os.getenv("REDIS_URL"),
 )
